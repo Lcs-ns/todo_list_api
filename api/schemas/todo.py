12ec559d00e68de7343837fb5735eb87889655a8
Lcs-ns <lucas.nas314@gmail.com>
2024-11-07 22:26:53 -0300

    feat: db model addition

--- api/schemas/todo.py
+++ api/schemas/todo.py
@@ -1,8 +1,9 @@
 from pydantic import BaseModel, Field
 from typing import Optional
 from tortoise.contrib.pydantic import pydantic_model_creator
+from api.models.todo import Todo
 
-GetTodo = pydantic_model_creator(None, name="Todo")
+GetTodo = pydantic_model_creator(Todo, name="Todo")
 
 class PostTodo(BaseModel):
     task: str = Field(..., max_length=100)
